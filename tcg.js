62124845222737992f7b458cd4ff59f9f7bf3388
Felix Kirchhoff <felix.kirchhoff@neuefische.de>
2022-06-29 13:33:18 +0200

    Refactor code and add name flag

--- tcg.js
+++ tcg.js
@@ -8,19 +8,26 @@ if (!filePath) {
   process.exit(1);
 }
 
+const colorFlagIndex = process.argv.indexOf("--color");
+const fileNameIndex = process.argv.indexOf("--name");
+
+const fileName =
+  (fileNameIndex > -1 && process.argv[fileNameIndex + 1]) ||
+  `tag-cloud-${new Date().toISOString().slice(0, 10)}`;
+
 const options = {
-  colorMode: "mono",
+  colorMode: colorFlagIndex > -1 ? process.argv[colorFlagIndex + 1] : null,
   backgroundColor: "#efefff",
 };
 
-options.colorMode = process.argv[process.argv.indexOf("--color") + 1] || "mono";
-
-const data = fs.readFileSync(filePath).toString();
-const tags = data.split(",").map((string) => string.trim());
+const tags = fs
+  .readFileSync(filePath)
+  .toString()
+  .split(",")
+  .map((string) => string.trim());
 
 const tagCloud = generateTagCloud(tags, options);
 
-fs.writeFileSync(
-  `tag-cloud-${new Date().toISOString().slice(0, 10)}.excalidraw`,
-  JSON.stringify(tagCloud, null, 2)
-);
+fs.writeFileSync(`${fileName}.excalidraw`, JSON.stringify(tagCloud, null, 2));
+
+console.log(`Success! Tags created: \n ${tags.join(", ")}`);
