60d060aa0abf5e08e802f1ab5f4a0d3f9b213b34
Felix Kirchhoff <felix.kirchhoff@neuefische.de>
2022-09-05 10:15:48 +0200

    feat: improve rainbow mode

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,24 @@
-export const getRandomColor = () =>
-  "#" +
-  Math.floor(Math.random() * 255 * 255 * 255)
-    .toString(16)
-    .padStart(6, "0");
+export const getRandomColor = () => {
+  const [r, g, b] = HSLToRGB(Math.floor(Math.random() * 360), 90, 65);
+  console.log({ r, g, b });
+  return (
+    "#" +
+    r.toString(16).padStart(2, "0") +
+    g.toString(16).padStart(2, "0") +
+    b.toString(16).padStart(2, "0")
+  );
+};
+
+const HSLToRGB = (h, s, l) => {
+  s /= 100;
+  l /= 100;
+  const k = (n) => (n + h / 30) % 12;
+  const a = s * Math.min(l, 1 - l);
+  const f = (n) =>
+    l - a * Math.max(-1, Math.min(k(n) - 3, Math.min(9 - k(n), 1)));
+  return [
+    Math.floor(255 * f(0)),
+    Math.floor(255 * f(8)),
+    Math.floor(255 * f(4)),
+  ];
+};
